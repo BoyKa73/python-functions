ff5a4353b2bd71d57d3fb85f60ff28883316bcb5
BoyKa73 <astradyne01@gmail.com>
2025-03-28 15:32:30 +0100

    Hinzugefügte erforderliche Parameter

--- def_createuserprofile1.py
+++ def_createuserprofile1.py
@@ -1,14 +1,14 @@
-def create_user_profile(**kwargs):
-    profile = {}
+def create_user_profile(name, email, **kwargs):
+    profile = {'name': name, 'email': email}  # Name und Email sind erforderlich
 
     for key, value in kwargs.items():
-        profile[key] = value
+        profile[key] = value                  #Optionale Attribute hinzufügen
 
     return profile
 
-
-alice = create_user_profile(name="Alice", age=33, city="New York")
-bob = create_user_profile(name="Bob", last_name="Smith", age=22, phone_number=1234567890)
+#Beispielaufruf
+alice = create_user_profile(name="Alice", email="alice@example.com", age=33, city="New York")
+bob = create_user_profile(name="Bob", email="bob@example.com", last_name="Smith", age=22, phone_number=1234567890)
 
 print(alice)
 print(bob)
